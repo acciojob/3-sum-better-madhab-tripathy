54663602dc1e473c34539f30971468995fd94cc7
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-05-16 20:10:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function threeSum(arr, target) {
 // write your code here
+	let min = Number.MAX_VALUE
+    let sorted = arr.sort((a, b) => a - b)
+    let ans = 0
+    for(let i = 0; i < sorted.length-2; i++) {
+        let start = i+1, end = sorted.length-1
+        while(start < end) {
+            let temp = arr[start]+arr[end]+arr[i]
+            if(Math.abs(target - temp) < min){
+                ans = temp
+                min = Math.abs(target - temp)
+            }
+            if(temp === target) {
+                return temp;
+            }
+            if(temp > target) {
+                end--;
+            }else{
+                start++;
+            }
+        }
+    }  
+    return ans;
   
 }
 
